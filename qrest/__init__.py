548e2aca93ead3299cf80ea8ae2c700d5f345a00
Pieter Swinkels <pieter.swinkels.ext@vegetableseeds.basf.com>
2021-05-10 10:28:02 +0200

    Back to development: 3.3.1

--- qrest/__init__.py
+++ qrest/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.3.0"
+__version__ = "3.3.1-dev"
 
 from .resource import JSONResource  # noqa: F401
 from .conf import APIConfig, ResourceConfig, BodyParameter, QueryParameter  # noqa: F401
